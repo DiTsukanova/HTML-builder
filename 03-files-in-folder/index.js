c582974af25c33079cff231d792d123e1621b808
Diana Tsukanova <dtsukanova5@gmail.com>
2022-11-07 21:39:26 +0300

    feat: add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+const folder = path.join(__dirname, 'secret-folder')
+
+fs.readdir(folder, (err, files) => {
+    if(err) {
+        console.log(err)
+    }
+    files.forEach(file => {
+        const pathOfFile = path.join(folder, file);
+        fs.stat(pathOfFile, (err, stats) => {
+            if(err) {
+                console.log(err)
+            }
+            if(stats.isFile()) {
+                const ext = path.extname(pathOfFile);
+                console.log(path.basename(pathOfFile, ext) + ' - ' + ext.slice(1, ext.length) + ' - ' + stats.size + 'b')
+            }
+        })
+    })
+})
